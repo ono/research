3b4ed89ceafe081c47a720e560e1ac679c46674e
Tatsuya Ono <ononoma@gmail.com>
2013-03-16 00:16:23 +0000

    played with Object reference

--- therubyracer-memory-leak/em_leak_ref.rb
+++ therubyracer-memory-leak/em_leak_ref.rb
@@ -2,6 +2,10 @@ require 'eventmachine'
 require 'json'
 require 'object_graph'
 
+# Use Irwin's find-references version of Ruby to see more object reference
+# information.
+# http://cirw.in/blog/find-references
+
 class EmLeak
   include EM::Deferrable
 
@@ -10,6 +14,7 @@ class EmLeak
     @hash = JSON.parse @text
 
     self.succeed
+    nil
   end
 
   def clear
@@ -18,40 +23,54 @@ class EmLeak
   end
 end
 
-def inspect_refs refs
+require 'pp'
+
+def inspect_refs
+  refs = ObjectSpace.find_references(
+      ObjectSpace.each_object(EmLeak).first
+    )
   refs.each do |ref|
     puts "#{ref.class}: #{ref.inspect}"
+    puts ref.methods.to_s
+    puts ref.class.ancestors.to_s
+  end
+end
+
+class Hoge
+  def self.hoge_tick &block
+    foo = []
+
+    # if you use push instead of <<, memory doesn't leak.
+    #foo.push(block)
+    foo << block
+    a = foo.shift
+    a.call
   end
 end
 
 def first_tick
-  i = 0
   leak = EmLeak.new
 
   leak.callback do
-    leak = nil if ARGV[0]
-  end
-  EM.next_tick { leak.run }
-end
+    puts "callback"
 
-puts "Thread: #{Thread.current.inspect}"
+    # if you enabile below, reference will be freed
+    #leak = nil
+  end
+
+  EM.next_tick { leak.run }
+
+  # You can reproduce leak with hoge_tick
+  # Hoge.hoge_tick { puts "test"; leak.run }
+end
 
 EM.run do
   first_tick
-  EM.next_tick do
-    puts "Thread: #{Thread.current.inspect}"
+  EM.add_timer(1) do
     ObjectSpace.garbage_collect
     ObjectGraph.new(ObjectSpace.each_object(EmLeak).first).view!
+    #inspect_refs
     EM.stop
   end
 end
-
-#if ObjectSpace.each_object(Foo).first
-#  refs = ObjectSpace.find_references(
-#      ObjectSpace.each_object(Foo).first
-#    )
-#else
-#  puts "Nothing"
-#end
-
 
